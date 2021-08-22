f6e2f2d7dd82467707d347f6006e110bfb58f8ee
Alexander Brandao <alex12a7x@gmail.com>
2021-08-21 21:57:55 -0300

    memoria

--- stats.js
+++ stats.js
@@ -3,4 +3,13 @@ const os = require('os')
 
 const { freemem, totalmem } = os
 
-console.log(parseInt(freemem()/1024/1024),parseInt(totalmem()/1024/1024))
+const total = parseInt(totalmem()/1024/1024)
+const mem = parseInt(freemem()/1024/1024)
+const percents = parseInt(mem/total *100)
+
+const stats = {
+    free: `${mem} MB`,
+    total: `${total} MB`,
+    usage: `${percents} %`
+}
+console.log(stats)
